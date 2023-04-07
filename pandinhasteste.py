681a61d6f2c19929f800762c3412efb30210886a
Theosly <theoamagalhaespc@gmail.com>
2023-04-07 15:53:51 -0300

    programas
    This reverts commit 3908d129a54deec6d66284a3711fe81ad3fbd33a.

--- pandinhasteste.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-dataset = {
-    'Sobremesas': ["Bolo", "Quindão", "Bolo de sorvete"],
-    'Tipo': ["Básico", "Básico", "Complexo"],
-    'Temperatura': ["Quente", "Gelado", "Gelado"]
-}
-lista = pd.DataFrame(dataset, index = ["Norma", "Theo", "Arthur"])
-print(lista)
